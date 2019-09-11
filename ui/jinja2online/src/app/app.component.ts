7811e15d496ef93e4fa1c999f256266958957ac9
George Tzianabos <george@tzianabos.com>
2019-09-11 15:13:58 -0400

    add github link

--- ui/jinja2online/src/app/app.component.ts
+++ ui/jinja2online/src/app/app.component.ts
@@ -4,10 +4,18 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
     <div class="container">
-      <div class="header">
-        <h1 class="header-title">
-          Jinja2online
-        </h1>
+      <div class="header row">
+        <div class="grow"></div>
+        <div class="grow">
+          <h1 class="header-title">
+            Jinja2online
+          </h1>
+        </div>
+        <div class="links grow column center">
+          <a class="row end link" href="https://github.com/georgetzianabos/jinja2online" target="_blank">
+            <svg width="24" height="24" viewBox="0 0 24 24"><use class="icon" href="#github"></use></svg>
+          </a>
+        </div>
       </div>
       <div class="content">
         <router-outlet></router-outlet>
